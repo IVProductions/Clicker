2f0bdbc2eec24e55526771bae00480210bf7f35d
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2014-02-28 00:37:36 -0800

    asdas

--- platforms/ios/www/app/controllers/indexCtrl.js
+++ platforms/ios/www/app/controllers/indexCtrl.js
@@ -1,8 +1,38 @@
 function indexCtrl($scope){
 
+
+	$scope.gold = 0;
+	$scope.gems = 0;
 	$scope.totalClicks = 0;
+	$scope.damage = 30;
+	$scope.hitMultiplier = 1.0;
+	$scope.upgMultiplier = 1.0;
+	$scope.nameMultiplier = 1.0;
+	$scope.basePower = 10;
+	$scope.totalPower = 10;
+
+	// SUPERMAN
+	$scope.health = 250;
+	$scope.armour = 20;
+	$scope.reward = 80;
+
+	$scope.currentHealth = $scope.health;
+
+	var percentage = 0;
 
 	$scope.click = function (){
+		// ADD TOTAL CLICKS
 		$scope.totalClicks++;
+
+		// DECREASE HEALTH
+		$scope.currentHealth = $scope.currentHealth - $scope.damage;
+		if ($scope.currentHealth <= 0) {
+			// INCREASE GOLD
+			$scope.gold = $scope.gold + $scope.reward;
+			// RESET HEALTH
+			$scope.currentHealth = $scope.health;
+		}
+		percentage = ($scope.currentHealth/$scope.health)*100;
+		$('.progressbar-cover').css('bottom' , percentage + '%');  // the cover controls the bar height
 	}
 }
---
+++ www/app/services/records.js
@@ -0,0 +1,85 @@
+Clicker.factory('records',function() {
+    var superheroes = [
+        {
+            "id": 1,
+            "name": "Superman",
+            "health": 100,
+            "armour": 0,
+            "reward": 2,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 2,
+            "name": "Superman",
+            "health": 200,
+            "armour": 2,
+            "reward": 10,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 3,
+            "name": "Superman",
+            "health": 300,
+            "armour": 4,
+            "reward": 20,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 4,
+            "name": "Superman",
+            "health": 500,
+            "armour": 8,
+            "reward": 50,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 5,
+            "name": "Superman",
+            "health": 800,
+            "armour": 12,
+            "reward": 100,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 6,
+            "name": "Superman",
+            "health": 1400,
+            "armour": 18,
+            "reward": 200,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 7,
+            "name": "Superman",
+            "health": 1680,
+            "armour": 22,
+            "reward": 300,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 8,
+            "name": "Superman",
+            "health": 2016,
+            "armour": 26,
+            "reward": 450,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 9,
+            "name": "Superman",
+            "health": 2419,
+            "armour": 31,
+            "reward": 1013,
+            "imageURL": "../img/superman.jpg"
+        },
+        {
+            "id": 10,
+            "name": "Superman",
+            "health": 2903,
+            "armour": 37,
+            "reward": 1013,
+            "imageURL": "../img/superman.jpg"
+        }
+    ]
+    return {superheroes:superheroes};
+})
