730f41c28c6ca2d26e733f85310475caad8c457d
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2014-02-27 18:35:11 -0800

    asda

--- www/app/controllers/indexCtrl.js
+++ www/app/controllers/indexCtrl.js
@@ -2,7 +2,20 @@ function indexCtrl($scope){
 
 	$scope.totalClicks = 0;
 
+	$scope.health = 250;
+	$scope.currentHealth = 250;
+
+	$scope.damage = 30;
+
+	var percentage = 0;
+
 	$scope.click = function (){
 		$scope.totalClicks++;
+		$scope.currentHealth = $scope.currentHealth - $scope.damage;
+		if ($scope.currentHealth <= 0) {
+			$scope.currentHealth = $scope.health;
+		}
+		percentage = ($scope.currentHealth/$scope.health)*100;
+		$('.progressbar-cover').css('bottom' , percentage + '%');  // the cover controls the bar height
 	}
 }
