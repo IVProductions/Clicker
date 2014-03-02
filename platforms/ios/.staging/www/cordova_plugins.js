d1f158b6364a5b4d7b39faa1b01133c5983c676d
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2014-03-01 17:30:03 -0800

    sds

--- platforms/ios/.staging/www/cordova_plugins.js
+++ platforms/ios/.staging/www/cordova_plugins.js
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "window.StatusBar"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.plugins.StatusBar/www/StatusBar.js",
+        "id": "org.apache.cordova.plugins.StatusBar.StatusBar",
+        "merges": [
+            "plugins.statusBar"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.statusbar": "0.1.3"
+    "org.apache.cordova.statusbar": "0.1.3",
+    "org.apache.cordova.plugins.StatusBar": "0.1.0"
 }
 // BOTTOM OF METADATA
 });
