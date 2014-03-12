3438cd3dbb77c25c6bf55bc361b6b5393ac0ef79
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2014-03-12 10:01:50 -0700

    asda

--- www/app/controllers/gameCtrl.js
+++ www/app/controllers/gameCtrl.js
@@ -687,6 +687,46 @@ function gameCtrl($scope, $location, records, statsRecords){
 		}
 	}
 
+	function measureText(txt, font) {
+	    var id = 'text-width-tester',
+	        $tag = $('#' + id);
+	    if (!$tag.length) {
+	        $tag = $('<span id="' + id + '" style="display:none;font:' + font + ';">' + txt + '</span>');
+	        $('body').append($tag);
+	    } else {
+	        $tag.css({font:font}).html(txt);
+	    }
+	    return {
+	        width: $tag.width(),
+	        height: $tag.height()
+	    }
+	}
+
+	function shrinkToFill(input, fontSize, fontWeight, fontFamily) {
+	    var $input = $(input),
+	        txt = $input.val(),
+	        maxWidth = $input.width() + 5, // add some padding
+	        font = fontWeight + " " + fontSize + "px " + fontFamily;
+	    // see how big the text is at the default size
+	    var textWidth = measureText(txt, font).width;
+	    if (textWidth > maxWidth) {
+	        // if it's too big, calculate a new font size
+	        // the extra .9 here makes up for some over-measures
+	        fontSize = fontSize * maxWidth / textWidth * .9;
+	        font = fontWeight + " " + fontSize + "px " + fontFamily;
+	        // and set the style on the input
+	        $input.css({font:font});
+	    } else {
+	        // in case the font size has been set small and 
+	        // the text was then deleted
+	        $input.css({font:font});
+	}
+
+	$('.allcaps').keyup(function() {
+        shrinkToFill(this, 20, "", "Georgia, serif");
+    })
+
+
 	$scope.changeView = function(view){
         $location.path(view);
     }
